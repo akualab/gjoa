0244ad539bf88ca7fd7abac7aed49d6ad099bbb6
Leo Neumeyer <leoneu@apache.org>
2013-09-07 10:47:59 -0700

    Implement alpha recursion.

--- model/hmm/hmm.go
+++ model/hmm/hmm.go
@@ -12,7 +12,8 @@ import (
 	"bitbucket.org/akualab/gjoa/model"
 	"code.google.com/p/biogo.matrix"
 	"fmt"
-	"github.com/golang/glog"
+	"math"
+	//"github.com/golang/glog"
 )
 
 const ()
@@ -29,6 +30,7 @@ type HMM struct {
 	// State-transition probability distribution matrix. [nstates x nstates]
 	// a(i,j) = P[q(t+1) = j | q(t) = i]; 0 <= i,j <= N-1
 	// when transitions between states are not allowed: a(i,j) = 0
+	// => saved in log domain.
 	transProbs *matrix.Dense
 
 	// Observation probability distribution functions. [nstates x 1]
@@ -37,6 +39,7 @@ type HMM struct {
 
 	// Initial state distribution. [nstates x 1]
 	// π(i) = P[q(0) = i]; 0<=i<N
+	// => saved in log domain.
 	initialStateProbs *matrix.Dense
 
 	// Complete HMM model.
@@ -71,9 +74,9 @@ func NewHMM(transProbs, initialStateProbs *matrix.Dense, obsModels []model.Model
 //
 //
 // 1. Initialization: α(i,0) =  π(i) b(i,o(0)); 0<=i<N
-// 2. Induction:      α(j,t+1) =  sum_{n=i}^{N-1}[α(i,t)a(i,j)] b(j,o(t+1)); 0<=t<T-1; 0<=j<N
+// 2. Induction:      α(j,t+1) =  sum_{i=0}^{N-1}[α(i,t)a(i,j)] b(j,o(t+1)); 0<=t<T-1; 0<=j<N
 // 3. Termination:    P(O/Φ) = sum_{i=0}^{N-1} α(i,T-1)
-func (hmm *HMM) ComputeAlpha(observations *matrix.Dense) (e error) {
+func (hmm *HMM) alpha(observations *matrix.Dense) (α *matrix.Dense, logProb float64, e error) {
 
 	// expected num rows: N = nstates
 	// expected num cols: T
@@ -86,12 +89,46 @@ func (hmm *HMM) ComputeAlpha(observations *matrix.Dense) (e error) {
 
 	// Allocate log-alpha matrix.
 	// TODO: use a reusable data structure to minimize garbage.
-	α := matrix.MustDense(matrix.ZeroDense(N, T))
+	α = matrix.MustDense(matrix.ZeroDense(N, T))
 
-	// 1. Initialization.
-	//α.SetColumn(0, hmm.initialStateProbs.Column(0))
+	// 1. Initialization. Add in the log doman.
+	for i := 0; i < N; i++ {
+		α.Set(i, 0, hmm.initialStateProbs.At(i, 0)+hmm.obsModels[i].LogProb(ColumnAt(observations, 0)))
+	}
 
 	// 2. Induction.
+	for t := 0; t < T-1; t++ {
+		for j := 0; j < N; j++ {
 
+			var sum float64
+			for i := 0; i < N; i++ {
+				sum += math.Exp(α.At(i, t) + hmm.transProbs.At(i, j))
+			}
+			α.Set(j, t+1, math.Log(sum)+hmm.obsModels[j].LogProb(ColumnAt(observations, t+1)))
+		}
+	}
+
+	// 3. Termination.
+	var sum float64
+	for i := 0; i < N; i++ {
+		sum += math.Exp(α.At(i, T-1))
+	}
+	logProb = math.Log(sum)
 	return
+}
+
+// ColumnAt returns a *matrix.Dense column that is a copy of the values at column c of the matrix.
+// Column will panic with ErrIndexOutOfRange is c is not a valid column index.
+func ColumnAt(d *matrix.Dense, c int) *matrix.Dense {
+
+	sl := d.Column(c)
+	nrows := len(sl)
+
+	col := matrix.MustDense(matrix.ZeroDense(nrows, 1))
+
+	for i := 0; i < nrows; i++ {
+		col.Set(i, 0, sl[i])
+	}
+
+	return col
 }
--- model/hmm/hmm_test.go
+++ model/hmm/hmm_test.go
@@ -1,9 +1,7 @@
 package hmm
 
 import (
-	"bitbucket.org/akualab/gjoa/model"
-	"bitbucket.org/akualab/gjoa/model/gaussian"
-	"bitbucket.org/akualab/gjoa/model/hmm"
+	//"bitbucket.org/akualab/gjoa/model/gaussian"
 	"code.google.com/p/biogo.matrix"
 	//	"math/rand"
 	"testing"
@@ -11,56 +9,70 @@ import (
 
 // Tests
 
-func TestGaussian(t *testing.T) {
+func TestColumnAt(t *testing.T) {
 
-	// Gaussian 1.
-	mean1, em1 := matrix.NewDense([][]float64{{1}})
-	if em1 != nil {
-		t.Fatal(em1)
-	}
-	var1, ev1 := matrix.NewDense([][]float64{{1}})
-	if ev1 != nil {
-		t.Fatal(ev1)
-	}
-	g1, eg1 := gaussian.NewGaussian(1, mean1, var1, true, true, "g1")
-	if eg1 != nil {
-		t.Fatal(eg1)
+	mat, e := matrix.NewDense([][]float64{{10, 11, 12, 13}, {20, 21, 22, 23}, {30, 31, 32, 33}})
+	if e != nil {
+		t.Fatal(e)
 	}
 
-	// Gaussian 2.
-	mean2, em2 := matrix.NewDense([][]float64{{4}})
-	if em2 != nil {
-		t.Fatal(em2)
-	}
-	var2, ev2 := matrix.NewDense([][]float64{{4}})
-	if ev2 != nil {
-		t.Fatal(ev2)
-	}
-	g2, eg2 := gaussian.NewGaussian(1, mean1, var1, true, true, "g2")
-	if eg2 != nil {
-		t.Fatal(eg2)
-	}
+	col := ColumnAt(mat, 1)
 
-	initialStateProbs, esp := matrix.NewDense([][]float64{{0.8}, {0.2}})
-	if esp != nil {
-		t.Fatal(esp)
-	}
+	t.Logf("col: \n%+v", col)
+}
 
-	transProbs, etp := matrix.NewDense([][]float64{{0.9, 0.1}, {0.3, 0.7}})
-	if etp != nil {
-		t.Fatal(etp)
-	}
+func TestEvaluation(t *testing.T) {
+	/*
+		// Gaussian 1.
+		mean1, em1 := matrix.NewDense([][]float64{{1}})
+		if em1 != nil {
+			t.Fatal(em1)
+		}
+		var1, ev1 := matrix.NewDense([][]float64{{1}})
+		if ev1 != nil {
+			t.Fatal(ev1)
+		}
+		g1, eg1 := gaussian.NewGaussian(1, mean1, var1, true, true, "g1")
+		if eg1 != nil {
+			t.Fatal(eg1)
+		}
 
-	hmm, e := hmm.NewHMM(transProbs, initialStateProbs, []*gaussian.Gaussian{g1, g2})
+		// Gaussian 2.
+		mean2, em2 := matrix.NewDense([][]float64{{4}})
+		if em2 != nil {
+			t.Fatal(em2)
+		}
+		var2, ev2 := matrix.NewDense([][]float64{{4}})
+		if ev2 != nil {
+			t.Fatal(ev2)
+		}
+		g2, eg2 := gaussian.NewGaussian(1, mean1, var1, true, true, "g2")
+		if eg2 != nil {
+			t.Fatal(eg2)
+		}
 
-	//	p := g.LogProb(obs)
-	//t.Logf("Gaussian: %+v", g)
-	//	t.Logf("LogProb: %f", p)
-	//	t.Logf("Prob: %f", g.Prob(obs))
-	// -3.3818
+		initialStateProbs, esp := matrix.NewDense([][]float64{{0.8}, {0.2}})
+		if esp != nil {
+			t.Fatal(esp)
+		}
 
-	//	expected := -3.3818
-	//	if !model.Comparef64(expected, p) {
-	//		t.Errorf("Wrong LogProb. Expected: [%f], Got: [%f]", expected, p)
-	//	}
+		transProbs, etp := matrix.NewDense([][]float64{{0.9, 0.1}, {0.3, 0.7}})
+		if etp != nil {
+			t.Fatal(etp)
+		}
+
+		models := []*gaussian.Gaussian{g1, g2}
+		hmm, e := NewHMM(transProbs, initialStateProbs, models)
+
+		//	p := g.LogProb(obs)
+		//t.Logf("Gaussian: %+v", g)
+		//	t.Logf("LogProb: %f", p)
+		//	t.Logf("Prob: %f", g.Prob(obs))
+		// -3.3818
+
+		//	expected := -3.3818
+		//	if !model.Comparef64(expected, p) {
+		//		t.Errorf("Wrong LogProb. Expected: [%f], Got: [%f]", expected, p)
+		//	}
+	*/
 }
