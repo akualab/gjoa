d87e6d193fd367fdcec283d777b2ff52541f54bc
Leo Neumeyer <leoneu@apache.org>
2015-03-31 18:13:33 -0700

    Implement alignent functions and tests.

--- model/align.go
+++ model/align.go
@@ -5,25 +5,125 @@
 
 package model
 
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/akualab/gjoa"
+)
+
 // ANode is an alignment node. Assumptions:
-//  * Root node (no parent) vovers the full interval.
+//  * Root node (no parent) covers the full interval.
 //  * A child node interval is included in the parent interval.
-//  * Concatenation of all the children intervals matches exactly the parent interval.
+//  * Concatenation of children intervals must match exactly the parent interval.
+//  * Tree must be balanced.
 type ANode struct {
-	// Start time (inclusive)
-	Start int
-	// End time (exclusive)
-	End int
+	// Start index (inclusive)
+	Start int `json:"s"`
+	// End index (exclusive)
+	End int `json:"e"`
 	// Name of unit being aligned.
-	Name string
-	// Pointer alignment one level up.
-	Parent *ANode
+	Name string `json:"n"`
+	// Value is an arbitrary object associated to an interval.
+	Value interface{} `json:"v"`
 	// Pointers to child alignments one level down.
-	Children []*ANode
+	Children []*ANode `json:"-"`
 }
 
-// Valid returns true if the alignment tree is valid.
-func (a *ANode) Valid() bool {
+// Alignment represents alignments as a sequence of ANodes by level.
+type Alignment [][]*ANode
+
+// NewANode creates a new ANode.
+func NewANode(start, end int, name string, value interface{}) *ANode {
+	return &ANode{Start: start, End: end, Name: name, Value: value, Children: []*ANode{}}
+}
+
+// Copy copies an ANode.
+// Children and Value fields are not copied.
+func (a *ANode) Copy() *ANode {
+
+	return &ANode{
+		Start:    a.Start,
+		End:      a.End,
+		Name:     a.Name,
+		Children: []*ANode{},
+	}
+}
+
+// Alignment returns alignments by level. The first slice index corresponds to a level
+// from 0 to max depth. The second index is the nth contiguous ANode at that level.
+// The ANodes are NOT copied. Make explicit copies if you need an independent set of ANodes.
+func (a *ANode) Alignment() Alignment {
+
+	bl := &byLevel{data: [][]*ANode{}}
+	root := a.Copy() // dummy root
+	root.Children = []*ANode{a}
+	root.nav(bl)
+	return bl.data
+}
+
+type byLevel struct {
+	data [][]*ANode
+}
+
+func (a *ANode) nav(ss *byLevel) (*ANode, int) {
+	level := 0
+	if len(a.Children) == 0 {
+		return a, 0
+	}
+	for _, v := range a.Children {
+		var child *ANode
+		child, level = v.nav(ss)
+		if level+1 > len(ss.data) {
+			ss.data = append(ss.data, []*ANode{})
+		}
+		ss.data[level] = append(ss.data[level], child)
+	}
+	return a, level + 1
+}
+
+// Level retrurns the level of the ANode. Level zero corresponds to the leaves.
+func (a *ANode) Level() int {
+	if len(a.Children) == 0 {
+		return 0
+	}
+	return a.Children[0].Level() + 1
+}
+
+// AppendChild creates a new ANode and appends to the receiver node.
+// The start index of the new node equals the end index of the last child.
+// Will panic if end < start OR end > parent's end.
+func (a *ANode) AppendChild(end int, name string) *ANode {
+
+	start := a.Start
+	if len(a.Children) != 0 {
+		start = a.Children[len(a.Children)-1].End
+	}
+	if end < start {
+		err := fmt.Errorf("end index [%d] must be greater than start index [%d]", end, start)
+		panic(err)
+	}
+	if end > a.End {
+		err := fmt.Errorf("end index [%d] must be less or equal than parent end index [%d]", end, a.End)
+		panic(err)
+	}
+
+	child := &ANode{
+		Start: start,
+		End:   end,
+		Name:  name,
+	}
+
+	a.Children = append(a.Children, child)
+	return child
+}
+
+// IsValid returns true if the alignment subtree is valid.
+func (a *ANode) IsValid() bool {
+	return a.checkIntervals() && a.isBalanced()
+}
+
+func (a *ANode) checkIntervals() bool {
 
 	if a.Children == nil {
 		return true
@@ -32,17 +132,33 @@ func (a *ANode) Valid() bool {
 	end := a.End
 	last := start
 	for _, v := range a.Children {
-		if !v.Valid() {
+		if !v.checkIntervals() {
 			return false
-		} else {
-			if v.Start != last {
-				return false
-			}
-			last = v.End
 		}
+		if v.Start != last {
+			return false
+		}
+		last = v.End
 	}
 	if last != end {
 		return false
+	}
+	return true
+}
+
+func (a *ANode) isBalanced() bool {
+
+	var prev int
+	for i, child := range a.Children {
+		level := child.Level()
+		if i == 0 {
+			prev = level
+			continue
+		}
+		if prev != level {
+			return false
+		}
+		prev = level
 	}
 	return true
 }
@@ -52,4 +168,56 @@ type Aligner interface {
 
 	// Alignment info.
 	Alignments() []ANode
+}
+
+// ToJSON returns a json string.
+func (a *ANode) ToJSON() (string, error) {
+	var b bytes.Buffer
+	err := gjoa.WriteJSON(&b, a)
+	return b.String(), err
+}
+
+// String prints an ANode.
+func (a *ANode) String() string {
+	s, err := a.ToJSON()
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
+
+// Tree converts an Alignment object to an alignment tree.
+// ANodes are NOT copied from the alignment data, instead the Children
+// fields are set to point to the children nodes.
+func (a Alignment) Tree() *ANode {
+	depth := len(a)
+	for level := depth - 2; level >= 0; level-- {
+		i := 0
+		var parent *ANode
+		for _, node := range a[level] {
+			if i == 0 || (node.Start == parent.End) {
+				parent = a[level+1][i]
+				parent.Children = []*ANode{}
+				i++
+			}
+			parent.Children = append(parent.Children, node)
+		}
+	}
+	return a[depth-1][0]
+}
+
+// ToJSON returns a json string.
+func (a Alignment) ToJSON() (string, error) {
+	var b bytes.Buffer
+	err := gjoa.WriteJSON(&b, a)
+	return b.String(), err
+}
+
+// String prints alignments.
+func (a Alignment) String() string {
+	s, err := a.ToJSON()
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
--- model/align_test.go
+++ model/align_test.go
@@ -5,22 +5,100 @@
 
 package model
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/akualab/gjoa"
+)
 
 func TestValid(t *testing.T) {
 	var root *ANode
-	a := &ANode{Start: 0, End: 3, Parent: root}
-	b := &ANode{Start: 3, End: 8, Parent: root}
-	c := &ANode{Start: 8, End: 10, Parent: root}
+	a := &ANode{Start: 0, End: 3}
+	b := &ANode{Start: 3, End: 8}
+	c := &ANode{Start: 8, End: 10}
 	root = &ANode{Start: 0, End: 10, Children: []*ANode{a, b, c}}
-	if !root.Valid() {
+	if !root.IsValid() {
 		t.Fatal("expected alignment tree to be valid but is not")
 	}
 
-	x := &ANode{Start: 3, End: 4, Parent: b}
-	y := &ANode{Start: 5, End: 8, Parent: b}
+	x := &ANode{Start: 3, End: 5}
+	y := &ANode{Start: 5, End: 8}
 	b.Children = []*ANode{x, y}
-	if root.Valid() {
-		t.Fatal("expected alignment tree to be invalid but is valid")
+	if root.isBalanced() {
+		t.Fatal("expected alignment tree to be unbalanced")
+	}
+	if !root.checkIntervals() {
+		t.Fatal("expected alignment tree to have correct intervals but it doesn't")
+	}
+}
+
+func makeTree() *ANode {
+	root := NewANode(0, 15, "root", nil)
+	a := root.AppendChild(3, "a")
+	b := root.AppendChild(8, "b")
+	c := root.AppendChild(15, "c")
+
+	a.AppendChild(3, "a0")
+
+	b.AppendChild(4, "b0")
+	b.AppendChild(6, "b1")
+	b.AppendChild(8, "b2")
+
+	c.AppendChild(11, "c0")
+	c.AppendChild(15, "c1")
+	return root
+}
+func TestAppend(t *testing.T) {
+
+	root := makeTree()
+	if !root.IsValid() {
+		t.Fatal("alignment tree is invalid")
+	}
+
+	level := root.Level()
+	if level != 2 {
+		t.Fatalf("expected level 2, got level %d", level)
+	}
+
+	for _, child := range root.Children {
+		level := child.Level()
+		if level != 1 {
+			t.Fatalf("expected level 1, got level %d", level)
+		}
+	}
+
+	al := root.Alignment()
+	t.Log(al)
+	tree := al.Tree()
+	t.Log(tree.checkIntervals())
+	t.Log(tree.isBalanced())
+	t.Log(tree.IsValid())
+	t.Log(tree)
+}
+
+func TestWriteRead(t *testing.T) {
+
+	root := makeTree()
+	al := root.Alignment()
+	fn := filepath.Join(os.TempDir(), "align.json")
+	gjoa.WriteJSONFile(fn, al)
+	t.Logf("Wrote to temp file: %s\n", fn)
+
+	var al2 Alignment
+	gjoa.ReadJSONFile(fn, &al2)
+	t.Log(al2)
+
+	for level := range al {
+		if len(al[level]) != len(al2[level]) {
+			t.Fatalf("length for level [%d] does not match - expected %d, got %d", level, len(al[level]), len(al2[level]))
+		}
+		for i, a := range al[level] {
+			b := al2[level][i]
+			if a.Start != b.Start || a.End != b.End || a.Name != b.Name {
+				t.Fatalf("nodes don't match - expected %s, got %s", a, b)
+			}
+		}
 	}
 }
